5a2daac7340db1fcc5ce9d0533e5890fcc123976
Pedro-ls <pedrohenriquedasilva204@gmail.com>
2020-11-27 13:03:58 -0200

    primeiro envio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Buttom from './components/buttoms/Buttom';
+import FormUser from './components/forms/FormUser';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>
+                    Olá Mundo!!!
+            </h1>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
+                    <Buttom title="Enviar" type="teste" style="teste" />
+                </a>
+            </header>
+            <FormUser />
+        </div>
+    );
 }
 
 export default App;
